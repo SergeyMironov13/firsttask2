245141104df1b5b342aea68267aab36c47182ec8
SergeyMironov13 <sermironov13@gmail.com>
2025-07-19 16:34:10 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        int restof = income-spending;
+        System.out.println(" при доходах в " + income + " и расходах в " + spending + " останется на счету " + restof);
+    }
+}
